b981c06c2ed3aa1609e7ac685e7ebfd31151e2b7
czyk <aron.scheffczyk@gmail.com>
2020-04-21 23:53:17 +0200

    added webpack-dev-server for development; added npm scripts

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,32 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const path = require('path')
+
 module.exports = {
     entry: './src/client/app.js',
+    mode: 'development',
+    devtool: 'sourcemap',
+    stats: 'verbose',
     output: {
         filename: 'main.js',
         path: path.resolve('./builds')
     },
-    rules: [
-
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.scss$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html"
+        })
     ]
 }
